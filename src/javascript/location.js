b73e1789c67cc2dbdbacc02b25d0ace33d062e8b
Jack Holdsworth <jackch90@gmail.com>
2020-04-20 22:06:04 +0100

    dotenv-webpack plugin install for geolocation api keys

--- src/javascript/location.js
+++ src/javascript/location.js
@@ -1,13 +1,25 @@
 const shareLocation = document.getElementById('shareLocation')
 
-function displayLocation(position) {
-    const long = position.coords.longitude;
-    const lat = position.coords.latitude;
-    console.log(`${long}, ${lat}`)
+function geoLookup(long, lat) {
+    const url = `https://photon.komoot.de/reverse?lon=${long}&lat=${lat}`
+    // const url = `https://maps.googleapis.com/maps/api/geocode/json?latlng=${lat},${long}&key=${process.env.GOOGLE_API_KEY}`
+    console.log(url)
+    fetch(url)
+        .then(response => (response.json()))
+        .then(data => {
+            console.log(data);
+        })
 }
+
+function displayLocation(position) {
+    let long = position.coords.longitude;
+    let lat = position.coords.latitude;
+    console.log(`Long: ${long} Lat: ${lat}`);
+    geoLookup(long, lat);
+};
 
 function findLocation() {
     navigator.geolocation.getCurrentPosition(displayLocation);
-}
+};
 
 shareLocation.addEventListener('click', findLocation);
