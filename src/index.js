22582d25a0e46daae9ddf63de6657876227dc2b3
Jack Holdsworth <jackch90@gmail.com>
2020-04-20 18:04:39 +0100

    moved date to date.js

--- src/index.js
+++ src/index.js
@@ -1,21 +1,4 @@
-import {test} from './javascript/test.js'
+import {insertDate} from './javascript/date.js'
 import './styles/main.scss';
-
-test();
-
-const timer = document.getElementById('time')
-const dateToday = document.getElementById('today')
-
-function insertDate() {
-    const date = new Date
-    let hour = date.getHours();
-    let seconds = date.getSeconds();
-    let minutes = date.getMinutes();
-    if (minutes < 10) { minutes = `0${minutes}` }
-    if (seconds < 10) { seconds = `0${seconds}` }
-    dateToday.innerHTML = `${date.getUTCDate()} ${date.getUTCMonth() + 1}`
-    timer.innerHTML = `${hour}:${minutes}:${seconds}`
-    setTimeout(insertDate, 1000);
-}
 
 insertDate();
---
+++ src/javascript/date.js
@@ -0,0 +1,16 @@
+const timer = document.getElementById('time')
+const dateToday = document.getElementById('today')
+
+function insertDate() {
+    const date = new Date
+    let hour = date.getHours();
+    let seconds = date.getSeconds();
+    let minutes = date.getMinutes();
+    if (minutes < 10) { minutes = `0${minutes}` }
+    if (seconds < 10) { seconds = `0${seconds}` }
+    dateToday.innerHTML = `${date.getUTCDate()}/${date.getUTCMonth() + 1}/${date.getUTCFullYear()}`
+    timer.innerHTML = `${hour}:${minutes}:${seconds}`
+    setTimeout(insertDate, 1000);
+}
+
+export { insertDate };
